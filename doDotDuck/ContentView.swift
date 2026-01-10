12fd1b1679aed29b5dc34538347cb6fac2c3db57
wimukthiudana-a <wimukthiudana@gmail.com>
2026-01-10 11:34:46 +0530

    Initial Commit

---
+++ doDotDuck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  doDotDuck
+//
+//  Created by cobsccomp24.2p-021 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ doDotDuck/doDotDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  doDotDuckApp.swift
+//  doDotDuck
+//
+//  Created by cobsccomp24.2p-021 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct doDotDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
